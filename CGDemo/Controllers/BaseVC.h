d6c4359437fd2fdda5b56d1c9cb956488a7b4a73
568601436 <chengang@OSellMobiledeiMac.local>
2015-11-20 14:35:27 +0800

    修改状态栏颜色

---
+++ CGDemo/Controllers/BaseVC.h
@@ -0,0 +1,13 @@
+//
+//  BaseVC.h
+//  CGDemo
+//
+//  Created by OSellMobile on 15/11/20.
+//  Copyright © 2015年 CGMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseVC : UIViewController
+
+@end
---
+++ CGDemo/Controllers/Home/HomeVC.h
@@ -0,0 +1,13 @@
+//
+//  HomeVC.h
+//  CGDemo
+//
+//  Created by OSellMobile on 15/11/20.
+//  Copyright © 2015年 CGMobile. All rights reserved.
+//
+
+#import "BaseVC.h"
+
+@interface HomeVC : BaseVC
+
+@end
